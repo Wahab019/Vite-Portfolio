2ec4b9e9079886bcc24e969f0b4c57e6497d1d13
AbdulWahab Lawal <lawalabdulwahab9@gmail.com>
2023-11-13 00:06:36 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './components/Header';
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-// import { useEffect, useState } from 'react';
+
 
 
 import Footer from './components/Footer';
@@ -48,54 +48,11 @@ function App() {
 
   
 
-    // const [mousePosition, setMousePosition] = useState({
-    //     x: 0,
-    //     y: 0
-    // });
-    // const [cursorVariant, setCursorVariant] = useState("default");
     
-    
-    // useEffect(() => {
-    //     const mouseMove = e => {
-    //       setMousePosition({
-    //         x: e.clientX,
-    //         y: e.clientY
-    //       })
-    //     }
-    
-    //     window.addEventListener("mousemove", mouseMove);
-    
-    //     return () => {
-    //       window.removeEventListener("mousemove", mouseMove);
-    //     }
-    // }, []);
-    
-    // const variants = {
-    //     default: {
-    //       x: mousePosition.x - 15,
-    //       y: mousePosition.y - 15,
-    //       border: "2px solid white",
-    //     },
-    //     text: {
-    //       height: 90,
-    //       width: 90,
-    //       x: mousePosition.x - 45,
-    //       y: mousePosition.y - 45,
-    //       backgroundColor: "#C0C0C07F",
-    //       mixBlendMode: "difference"
-    //     }
-    // }
-    
-    // const textEnter = () => setCursorVariant("text");
-    // const textLeave = () => setCursorVariant("default");
 
   return (
     <>
-        {/* <motion.div
-          className='bg-transparent h-[30px] w-[30px] z-50 rounded-[50%] fixed top-0 left-0 pointer-events-none'
-          variants={variants}
-          animate={cursorVariant}
-        /> */}
+      
       
     
         <RouterProvider router={router} />
